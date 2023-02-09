b5ab989960e51ee9e99a94e088f1953022329854
YJung <rladuswjd0303@naver.com>
2023-02-09 15:02:48 +0900

    firebase 초기 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjAEDXAcIw53-jZaDALzBW3ZqJmFB2In8",
+  authDomain: "consumption-2dbcb.firebaseapp.com",
+  projectId: "consumption-2dbcb",
+  storageBucket: "consumption-2dbcb.appspot.com",
+  messagingSenderId: "354685606827",
+  appId: "1:354685606827:web:a5a3fac3c8b8689a7310ee",
+};
+
+export default firebase.initializeApp(firebaseConfig);
